fd7fb813c2cfa953a6f989f7cfe6b9d0b989dbcf
Ihor Savaryn <ihor.savaryn@gmail.com>
2015-03-14 09:00:57 +0200

    update tbody

--- students/views.py
+++ students/views.py
@@ -1,9 +1,29 @@
+#_*_ coding: utf-8 _*_
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 #Students
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+    {'id':1,
+    'first_name': u'Корост',
+    'last_name': u'Андрій',
+    'ticket': 2123,
+    'image': 'img/1.jpg'},
+    {'id':2,
+    'first_name': u'Саварин',
+    'last_name': u'Сергій',
+    'ticket': 2100,
+    'image': 'img/2.jpg'},
+    {'id':3,
+    'first_name': u'Саварин',
+    'last_name': u'Ігор',
+    'ticket': 1034,
+    'image': 'img/3.jpg'},
+    )
+    return render(request, 'students/students_list.html', 
+	{'students': students})
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
 def students_edit(request, sid):
